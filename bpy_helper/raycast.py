65448f5847067a6b482d330439ad88e394c2beac
Iyad Ahmed <iyadahmed@cgonfire.com>
2022-01-13 12:33:30 +0200

    Update raycast module

--- bpy_helper/raycast.py
+++ bpy_helper/raycast.py
@@ -1,5 +1,27 @@
+from dataclasses import dataclass
+
 import bpy
 from bpy_extras import view3d_utils
+from mathutils import Matrix, Vector
+
+
+@dataclass(frozen=True)
+class RayHit:
+    is_hit: bool
+    location: Vector
+    normal: Vector
+    polygon_index: int
+    obj: bpy.types.Object
+    obj_matrix: Matrix
+
+    def __bool__(self):
+        return self.is_hit
+
+
+def ray_cast_scene(context: bpy.types.Context, ray_origin, ray_target):
+    dg = context.evaluated_depsgraph_get()
+    is_hit, location, normal, polygon_index, obj, obj_matrix = context.scene.ray_cast(dg, ray_origin, ray_target)
+    return RayHit(is_hit, location, normal, polygon_index, obj, obj_matrix)
 
 
 def region_2d_to_ray_view3d(context: bpy.types.Context, x: int, y: int):
