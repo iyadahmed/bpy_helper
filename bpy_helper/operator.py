799ea9875405148b319265669688949fd51a7f5e
Iyad Ahmed <iyadahmed@cgonfire.com>
2022-01-13 12:58:53 +0200

    Update operator module

--- bpy_helper/operator.py
+++ bpy_helper/operator.py
@@ -1,11 +1,11 @@
 import traceback
-from typing import Callable, Optional, Protocol, Set, Union
+from typing import Callable, Optional, Set
 
 import bpy
 
 
-class BaseMultiStateModal(Protocol):
-    _modal_handler: Callable[[bpy.types.Context, bpy.types.Event], Set[str]]
+class MultiStateModal(bpy.types.Operator):
+    _modal_handler: Callable[[bpy.types.Context, bpy.types.Event], Set[str]] = None
 
     def setup(self, context) -> None:
         ...
@@ -16,7 +16,7 @@ class BaseMultiStateModal(Protocol):
     def pre_modal_handler(self, context, event) -> Optional[Set[str]]:
         """an event handler that is executed before modal handler,
         if it returns {"RUNNING_MODAL"}, then _modal_handler will be executed after"""
-        ...
+        return {"RUNNING_MODAL"}
 
     def set_modal_handler(self, modal_handler: Callable[[bpy.types.Context, bpy.types.Event], Set[str]]):
         self._modal_handler = modal_handler
@@ -26,16 +26,13 @@ class BaseMultiStateModal(Protocol):
         context.window_manager.modal_handler_add(self)
         return {"RUNNING_MODAL"}
 
-    def modal(
-        self: Union[bpy.types.Operator, "BaseMultiStateModal"],
-        context: bpy.types.Context,
-        event: bpy.types.Event,
-    ):
+    def modal(self, context: bpy.types.Context, event: bpy.types.Event):
         try:
             context.area.tag_redraw()
             retval = self.pre_modal_handler(context, event)
             if retval == {"RUNNING_MODAL"}:
-                retval = self._modal_handler(context, event)
+                if self._modal_handler:
+                    retval = self._modal_handler(context, event)
 
             if retval in ({"FINISHED"}, {"CANCELLED"}):
                 self.cleanup(context)
