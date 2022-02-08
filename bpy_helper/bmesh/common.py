6717347edad071afc745759fcf38bf71dc4e7b4b
Iyad Ahmed <iyadahmed@cgonfire.com>
2022-02-08 15:30:08 +0200

    BMesh: create cylinder that works for 2.9x and 3.x

--- bpy_helper/bmesh/common.py
+++ bpy_helper/bmesh/common.py
@@ -1,4 +1,5 @@
 import bpy
+from mathutils import Matrix
 
 import bmesh
 
@@ -10,3 +11,20 @@ def bmesh_to_object(bm: bmesh.types.BMesh, name: str = ""):
     obj: bpy.types.Object = bpy.data.objects.new(name, mesh)
     obj.use_fake_user = True
     return obj
+
+
+def create_cylinder(bm: bmesh.types.BMesh, radius: float, height: float, mat: Matrix, calc_uvs: bool = False):
+    bmesh.ops.create_cone(
+        bm,
+        cap_ends=True,
+        cap_tris=True,
+        segments=64,
+        **(
+            {"diameter1": radius, "diameter2": radius}
+            if bpy.app.version < (3, 0, 0)
+            else {"radius1": radius, "radius2": radius}
+        ),
+        depth=height,
+        matrix=mat,
+        calc_uvs=calc_uvs,
+    )
