31a0ab3aa0a84e29ccfc7aaa50747e44c7af2d2f
Iyad Ahmed <iyadahmed@cgonfire.com>
2022-02-16 12:48:06 +0200

    Modal operator numeric input helper

--- bpy_helper/operator.py
+++ bpy_helper/operator.py
@@ -44,3 +44,48 @@ class MultiStateModal(bpy.types.Operator):
             self.report({"ERROR"}, traceback.format_exc())
             self.cleanup(context)
             return {"FINISHED"}
+
+
+NUM_EVENT_TO_STR = {
+    "ZERO": "0",
+    "ONE": "1",
+    "TWO": "2",
+    "THREE": "3",
+    "FOUR": "4",
+    "FIVE": "5",
+    "SIX": "6",
+    "SEVEN": "7",
+    "EIGHT": "8",
+    "NINE": "9",
+    "NUMPAD_0": "0",
+    "NUMPAD_1": "1",
+    "NUMPAD_2": "2",
+    "NUMPAD_3": "3",
+    "NUMPAD_4": "4",
+    "NUMPAD_5": "5",
+    "NUMPAD_6": "6",
+    "NUMPAD_7": "7",
+    "NUMPAD_8": "8",
+    "NUMPAD_9": "9",
+}
+
+
+def modal_update_float_input_string_unsigned(op: bpy.types.Operator, event: bpy.types.Event, string_property_name: str):
+    assert hasattr(op, string_property_name)
+    old_str = getattr(op, string_property_name)
+    new_str = old_str
+    if (event.type in NUM_EVENT_TO_STR.keys()) and (event.value == "PRESS"):
+        new_str = old_str + NUM_EVENT_TO_STR[event.type]
+
+    elif (event.type in ("PERIOD", "NUMPAD_PERIOD")) and (event.value == "PRESS") and ("." not in old_str):
+        new_str = old_str + "."
+
+    elif (event.type == "BACK_SPACE") and (event.value == "PRESS") and (len(old_str) > 0):
+        new_str = old_str[:-1]
+
+    setattr(op, string_property_name, new_str)
+    try:
+        return float(new_str)
+    except Exception:
+        setattr(op, string_property_name, "")
+        return None
