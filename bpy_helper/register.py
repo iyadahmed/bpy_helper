b6c3ca10738b054231b8908c842748a21fbc33bb
Iyad Ahmed <iyadahmed@cgonfire.com>
2022-02-26 09:23:44 +0200

    Replace ModuleRegisterHelper class with module_register_factory

--- bpy_helper/register.py
+++ bpy_helper/register.py
@@ -13,24 +13,22 @@ def cleanse_modules(parent_module_name):
             del sys.modules[module_name]
 
 
-class ModuleRegisterHelper:
-    def __init__(self, parent_module_name: str, module_names: List[str]) -> None:
-        self._parent_module_name = parent_module_name
-        self.modules = [importlib.import_module(f"{parent_module_name}.{name}") for name in module_names]
+# Similar to bpy.utils.register_submodule_factory
+def module_register_factory(parent_module_name: str, module_names: List[str]):
+    modules = [importlib.import_module(f"{parent_module_name}.{name}") for name in module_names]
 
-    def register(self):
-        for m in self.modules:
+    def register():
+        for m in modules:
             try:
                 m.register()
             except Exception:
                 print_exc()
 
-    def unregister(self):
-        for m in reversed(self.modules):
+    def unregister():
+        for m in reversed(modules):
             try:
                 m.unregister()
             except Exception:
                 print_exc()
 
-    def get_register_unregister(self):
-        return self.register, self.unregister
+    return register, unregister
