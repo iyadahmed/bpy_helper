3b5606f3b34d4fb2efb729ab811c52c94ac98cd4
possiblt ferret <100738243+possiblyFerret@users.noreply.github.com>
2022-03-08 10:09:14 +0700

    feature `rgb_to_hsv` and `hsv_to_rgb`
    a new color convertion mode, turning rgb to hsv and back

--- bpy_helper/color.py
+++ bpy_helper/color.py
@@ -11,3 +11,69 @@ def hex_to_rgb(color_str_hex: str):
 
 def rgb_to_hex(color: tuple[int, int, int]):
     return "#" + bytes(color).hex()
+
+
+def rgb_to_hsv(color: tuple[float, float, float]) -> tuple[float, float, float]:
+
+    r, g, b = color
+
+    maxc = max(r, g, b)
+    minc = min(r, g, b)
+
+    v = maxc
+
+    if minc == maxc:
+        return 0.0, 0.0, v
+
+    s = (maxc - minc) / maxc
+    rc = (maxc - r) / (maxc - minc)
+    gc = (maxc - g) / (maxc - minc)
+    bc = (maxc - b) / (maxc - minc)
+
+    if r == maxc:
+        h = bc - gc
+    elif g == maxc:
+        h = 2.0 + rc - bc
+    else:
+        h = 4.0 + gc - rc
+
+    h = (h / 6.0) % 1.0
+
+    return (h, s, v)
+
+
+def hsv_to_rgb(color: tuple[float, float, float]) -> tuple[float, float, float]:
+
+    h, s, v = color
+
+    if s == 0.0:
+        return v, v, v
+
+    i = int(h * 6.0)
+
+    f = (h * 6.0) - i
+
+    v1 = v * (1.0 - s)
+    v2 = v * (1.0 - s * f)
+    v3 = v * (1.0 - s * (1.0 - f))
+
+    i = i % 6
+
+    match i:
+        case 0:
+            return v, v3, v1
+
+        case 1:
+            return v2, v, v1
+
+        case 2:
+            return v1, v, v3
+
+        case 3:
+            return v1, v2, v
+
+        case 4:
+            return v3, v1, v
+
+        case _:
+            return v, v1, v2
